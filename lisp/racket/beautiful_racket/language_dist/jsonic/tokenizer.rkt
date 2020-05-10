909daca28a60cc0114aa07401d98f7bd90f4fead
K Rhoda <kelseydrhoda@gmail.com>
2020-05-10 10:31:04 -0600

    added tests to tokenizer

--- lisp/racket/beautiful_racket/language_dist/jsonic/tokenizer.rkt
+++ lisp/racket/beautiful_racket/language_dist/jsonic/tokenizer.rkt
@@ -1,6 +1,17 @@
 #lang br/quicklang
 (require brag/support racket/contract)
 
+(module+ test 
+    (require rackunit))
+
+(define (jsonic-token? x)
+    (or (eof-object? x) (token-struct? x)))
+
+(module+ test
+    (check-true (jsonic-token? eof))
+    (check-true (jsonic-token? (token 'A-TOKEN-STRUCT "hi")))
+    (check-false (jsonic-token? 42)))
+
 (define (make-tokenizer port)
     (define (next-token)
         (define jsonic-lexer 
@@ -15,7 +26,16 @@
         (jsonic-lexer port))
         next-token)
 
-(define (jsonic-token? x)
-    (or (eof-object x) (token-struct? x)))
+(provide (contract-out [make-tokenizer (input-port? . -> . (-> jsonic-token?))]))
 
-(provide (contract-out [make-tokenizer (input-port? . -> . (-> jsonic-token?))])
+(module+ test
+    (check-equal? 
+        (apply-tokenizer-maker make-tokenizer "// comment\n") 
+        empty)
+    (check-equal? 
+        (apply-tokenizer-maker make-tokenizer "@$ (+ 6 7) $@")
+        (list (token-struct 'SEXP-TOK " (+ 6 7) " #f #f #f #f #f)))
+    (check-equal?
+        (apply-tokenizer-maker make-tokenizer "hi")
+        (list (token-struct 'CHAR-TOK "h" #f #f #f #f #f)
+            (token-struct 'CHAR-TOK "i" #f #f #f #f #f))))
