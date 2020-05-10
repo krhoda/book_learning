1f0fd837543e30db4b76a34bc42f60ab734313d1
K Rhoda <kelseydrhoda@gmail.com>
2020-05-10 12:53:07 -0600

    bumped deps, finished jsonic colerer

--- lisp/racket/beautiful_racket/language_dist/jsonic/colorer.rkt
+++ lisp/racket/beautiful_racket/language_dist/jsonic/colorer.rkt
@@ -1,5 +1,8 @@
 #lang br
-(require brag/support syntax-color/racket-lexer)
+(require 
+    brag/support 
+    racket/contract
+    syntax-color/racket-lexer)
 
 (define jsonic-lexer
     (lexer 
@@ -18,7 +21,7 @@
 (define (color-jsonic port offset racket-coloring-mode?)
     ;;; Check for coloring of JSON or Racket...
     (cond
-        ;;; If JSON keep going until...
+        ;;; ...If JSON keep going until...
         [(or (not racket-coloring-mode?)
             ;;; ... a delimiter is seen, then switch!
             (equal? (peek-string 2 0 port) "$@"))
@@ -26,10 +29,28 @@
                 (jsonic-lexer port))
             (define switch-to-racket-mode (equal? str "@$"))
             (values str cat paren start end 0 switch-to-racket-mode)]
-        ;;; Otherwise evaluate the S-expression
+        ;;; ...Otherwise evaluate the S-expression
         [else
             (define-values (str cat paren start end)
                 (racket-lexer port))
             (values str cat paren start end 0 #t)]))
 
-(provide color-jsonic)
+(provide (contract-out [color-jsonic
+    (input-port? 
+    exact-nonnegative-integer? 
+    boolean? 
+    . -> . 
+    (values 
+        (or/c string? eof-object?)
+        symbol?
+        (or/c symbol? #f)
+        (or/c exact-positive-integer? #f)
+        (or/c exact-positive-integer? #f)
+        exact-nonnegative-integer?
+        boolean?))]))
+
+(module+ test
+    (require rackunit)
+    (check-equal? (values->list
+        (color-jsonic (open-input-string "x") 0 #f))
+        (list "x" 'string #f 1 2 0 #f)))
