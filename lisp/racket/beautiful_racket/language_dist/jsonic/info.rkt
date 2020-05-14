146bfa0532b91254e022c1c76758450ccad1e4d5
K Rhoda <kelseydrhoda@gmail.com>
2020-05-14 09:11:37 -0600

    started info.rkt

---
+++ lisp/racket/beautiful_racket/language_dist/jsonic/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+(define collection "jsonic")
+(define version "1.0")
+(define scribblings '(("scribblings/jsonic.scrbl")))
+;;; example test config.
+;;; (define test-omit-paths '("jsonic-test.rkt"))
+(define deps '("base"
+    "beautiful-racket-lib"
+    "brag-lib"
+    "draw-lib"
+    "gui-lib"
+    "rackunit-lib"
+    "syntax-color-lib"))
