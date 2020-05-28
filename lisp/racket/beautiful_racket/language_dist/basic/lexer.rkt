c2b1870516e62313d8924b8f65df743892c482eb
K Rhoda <kelseydrhoda@gmail.com>
2020-05-27 19:02:55 -0600

    concluded basic lexer

--- lisp/racket/beautiful_racket/language_dist/basic/lexer.rkt
+++ lisp/racket/beautiful_racket/language_dist/basic/lexer.rkt
@@ -1,20 +1,32 @@
 #lang br
 (require brag/support)
 
-(define-lex-abbrev digits (:+ (char-set "0123456789"))
+(define-lex-abbrev digits (:+ (char-set "0123456789")))
 
 (define basic-lexer
     (lexer-srcloc
         ;;; Catch meaningful whitespace first...
-        ["\n", (token 'NEWLINE lexeme)]
+        ["\n" (token 'NEWLINE lexeme)]
         ;;; ...Then the rest
         [whitespace (token lexeme #:skip? #t)]
-        ;;; Comments
+        ;;; Comments:
         [(from/stop-before "rem" "\n") (token 'REM lexeme)]
         ;;; Operators:
-        [(:or "print" "goto" "end" "+" ":" "l")
+        [(:or "print" "goto" "end" "+" ":" ";")
             (token lexeme lexeme)]
-        ;; TODO: Digits:
-        ))
+        ;;; One digit:
+        [digits (token 'INTEGER (string->number lexeme))]
+        ;;; A sequence of possible floats numbers
+        ;;; matches .456, 123.456, and 123:
+        [(:or (:seq (:? digits) "." digits)
+                (:seq digits "."))
+            (token 'DECIMAL (string->number lexeme))]
+        ;;; Strings:
+        [(:or (from/to "\"" "\"") (from/to "'" "'"))
+            (token 'STRING
+                ;;; Drop the quotes.
+                (substring lexeme 
+                    1
+                    (sub1 (string-length lexeme))))]))
 
 (provide basic-lexer)
