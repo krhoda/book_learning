18faa881693d33b1c4bde2dfc930abe19f8d78ee
K Rhoda <kelseydrhoda@gmail.com>
2020-05-29 17:35:34 -0600

    grammar and test complete

--- lisp/racket/beautiful_racket/language_dist/basic/parser.rkt
+++ lisp/racket/beautiful_racket/language_dist/basic/parser.rkt
@@ -1,27 +1,27 @@
 #lang brag
 
+;;; Everything busts without this at the top.
+b-program : [b-line] (NEWLINE [b-line])*
+
 ;;; NOTE: THE Following 
 ;;; b-line : b-line-num [b-statement (":" b-statement)*]
 ;;; Expands to:
 ;;; b-line : b-line-num 
     ;;; | b-line-num b-statement
     ;;; | b-line-num b-statement (":" b-statement)+
-
 ;;; Subtly different from the above by making the statements all optional.
 b-line : b-line-num [b-statement] (":" [b-statement])* [b-rem]
-
-b-program : [b-line] (NEWLINE [b-line])*
-
 b-line-num : INTEGER
-
 b-statement : b-end | b-print | b-goto
-
 b-rem : REM
-
 b-end : "end"
+b-print : "print" [b-printable] (";" [b-printable])*
+b-printable : STRING | b-expr
+b-goto : "goto" b-expr
 
-;;; TODO: WORK FROM HERE:
+;;; Aliasing to allow conversion between BASIC and Racket Math.
+b-expr : b-sum
+b-sum : b-number ("+" b-number)*
+b-number : INTEGER | DECIMAL
 
-b-print : "print" [b-printable]
-b-printable :
-b-goto :
+
