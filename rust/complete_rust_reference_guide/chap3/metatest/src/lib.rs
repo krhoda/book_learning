97643ff2df587518d31e084344b5c830d617982f
K Rhoda <kelseydrhoda@gmail.com>
2020-07-15 18:28:06 -0600

    tests and doc strings

---
+++ rust/complete_rust_reference_guide/chap3/metatest/src/lib.rs
@@ -0,0 +1,32 @@
+//! Single line module comments should look like this.
+
+/*!
+ * Multiline Module comments should look like this
+ */
+
+/// Single line Item Doc Strings should look like this.
+pub fn sum(a: i8, b: i8) -> i8 {
+    a + b
+}
+
+/** Multi line Item Doc Strings should look 
+ * like
+ * this
+ */
+pub fn unsum(a: i8, b: i8) -> i8 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    fn sum_inputs_outputs() -> Vec<((i8, i8), i8)> {
+        vec![((1, 1), 2), ((0, 0), 0), ((2, -2), 0)]
+    }
+
+    #[test]
+    fn test_sums() {
+        for (input, output) in sum_inputs_outputs() {
+            assert_eq!(crate::sum(input.0, input.1), output);
+        }
+    }
+}
