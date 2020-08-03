b91e2d0ecafd2e74d7049f3c2b155f77ee4cd25f
K Rhoda <kelseydrhoda@gmail.com>
2020-08-02 19:36:41 -0600

    began clojure for the Brave and True to (successfully) test CIDER config

---
+++ lisp/clojure/brave_and_true/chap_1/clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,13 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;;; as per the instructions:
+  (println "I'm a little teapot!"))
+
+(defn train
+  "It is a train"
+  []
+  (println "Choo choo!"))
