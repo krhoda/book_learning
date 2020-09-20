a864fcdf0c6662e855344f6b87f4bd0fac08fb21
K Rhoda <kelseydrhoda@gmail.com>
2020-09-20 17:11:05 -0600

    Added some IO to util.lisp

--- lisp/common_lisp/on_lisp/chap_4/util.lisp
+++ lisp/common_lisp/on_lisp/chap_4/util.lisp
@@ -234,3 +234,23 @@
 			 #'(lambda (&rest args)
 				 (apply #'rmapcar fn args))
 			 args)))
+
+;;; I/O:
+(defun readlist (&rest args)
+  (values (read-from-string
+		   (concatenate 'string
+						"("
+						(apply #'read-line args)
+						")"))))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop. ~%")
+  (loop
+	(let ((in (apply #'prompt args)))
+	  (if (funcall quit in)
+		  (return)
+		  (format *query-io* "~A~%" (funcall fn in))))))
