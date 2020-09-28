c9c9c194dd484b98cc195890da576d35bfcaf3d4
K Rhoda <kelseydrhoda@gmail.com>
2020-09-27 21:27:48 -0600

    added clarity to result/option combinators

--- rust/complete_rust_reference_guide/chap6/combinatorz/src/main.rs
+++ rust/complete_rust_reference_guide/chap6/combinatorz/src/main.rs
@@ -4,22 +4,34 @@ fn main() {
     let g_o: Option<i64> = Some(2);
     let b_o: Option<i64> = None;
 
-    let _ = g_r.as_ref().map(|x| println!("Will be positive one: {}", x));
+    let _ = g_r
+        .as_ref()
+        .map(|x| println!("Will be positive one: {}", x));
     // NOTE: Only for Result.
-    let _ = b_r.as_ref().map_err(|x| println!("Will be a negative statement: {}", x));
+    let _ = b_r
+        .as_ref()
+        .map_err(|x| println!("Will be a negative statement: {}", x));
 
     {
         // Works for both result and Option
+        // Takes success value, maps in.
         let my_two = g_r.as_ref().and_then(|x| Ok(x + x));
-        let my_not_two = b_o.as_ref().and_then(|x| Some(x));
         match my_two {
             Ok(x) => println!("This will print: {}", x),
-            Err(x) => println!("This won't: {}", x)
+            Err(x) => println!("This won't: {}", x),
         }
 
+        // Returns failure value
+        let my_negative_statement = b_r.as_ref().and_then(|x| Ok(x + x));
+        match my_negative_statement {
+            Err(x) => println!("This will print: {}", x),
+            Ok(x) => println!("This won't: {}", x),
+        }
+
+        let my_not_two = b_o.as_ref().and_then(|x| Some(x));
         match my_not_two {
+            None => println!("This will print in response to None"),
             Some(x) => println!("This won't print: {}", x),
-            None => println!("This will print in response to None")
         }
     }
 }
