0de2b216b1978c38d1166a4a17d2ce2de3087f13
K Rhoda <kelseydrhoda@gmail.com>
2020-10-18 15:28:24 -0600

    changed WASM rogue-like to use Crank.js w/typescript veruses weird hybird app or React

--- rust/wasm/part_2/rogue_wasm/app/src/index.tsx
+++ rust/wasm/part_2/rogue_wasm/app/src/index.tsx
@@ -1,13 +1,13 @@
 /** @jsx createElement */
-import { createElement } from "@bikeshaving/crank";
-import { renderer } from "@bikeshaving/crank/dom";
-import { Element } from "@bikeshaving/crank";
+import { createElement } from '@bikeshaving/crank';
+import { renderer } from '@bikeshaving/crank/dom';
+import { Element } from '@bikeshaving/crank';
 
-const appContainer = document.getElementById("app");
+const appContainer = document.getElementById('app');
 
 function Greeting(): Element {
   return <div>Hello world</div>;
-};
+}
 
 if (appContainer) {
   renderer.render(<Greeting />, appContainer);
