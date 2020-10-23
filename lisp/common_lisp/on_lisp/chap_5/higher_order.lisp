72952fbb723bdcdb638e2a01824ab452757fe4f0
K Rhoda <kelseydrhoda@gmail.com>
2020-10-22 23:57:20 -0600

    got through most of higher order chapter

--- lisp/common_lisp/on_lisp/chap_5/higher_order.lisp
+++ lisp/common_lisp/on_lisp/chap_5/higher_order.lisp
@@ -76,14 +76,13 @@ lst-two ; => (1 2 4) WHATS WRONG WITH DELETE-IF???
 (time (funcall slowid 1)) ; will run for 5 seconds.
 (time (funcall slowid 1)) ; will run for 0.000 seconds.
 
-; compose
-; can you tell I like haskell?
+; compose, limited to single arg excluding the original, single return including the original
 (defun compose (&rest fns)
   (if fns
 	  ; let over...
 	  (let ((fn1 (car (last fns)))
 			(fns (butlast fns)))
-	  ; lambda ...
+	  ; ... lambda
 	  #'(lambda (&rest args)
 		  (reduce #'funcall fns
 				  :from-end t
@@ -93,3 +92,107 @@ lst-two ; => (1 2 4) WHATS WRONG WITH DELETE-IF???
 (setq inc-list (compose #'(lambda (x) (* x x)) #'1+))
 (funcall inc-list 2) ; => 9
 (funcall (compose #'1+ #'find-if) #'oddp '(2 3 4)) ; => 4
+
+; the original example is a lot more uncomfortable...
+; ... but anyway, lets talk about puppies:
+; this form of code often appears:
+(mapcar #'(lambda (pup)
+			(if (spotted pup)
+				(adorable pup)
+				(vcute pup)))
+		puppies)
+; lets define this quite goofily:
+(setq puppies '(1 2 3 4 5))
+(defun spotted (x) (oddp x))
+(defun adorable (x) (print "ADORABLE"))
+(defun vcute (x) (print "V CUTE"))
+
+; Let's generalize that with function builders ...
+(defun fun-if (test then &optional else)
+  #'(lambda (x)
+	  (if (funcall test x)
+		  (funcall then x)
+		  (if else (funcall else x)))))
+
+
+; ... making the above renderable as:
+(mapcar (fun-if #'spotted #'adorable #'vcute) puppies)
+
+; function intersection, do ALL fns return true?
+(defun fun-ix (fn &rest fns)
+  (if (null fns)
+	  fn
+	  (let ((chain (apply #'fun-ix fns)))
+		#'(lambda (x)
+			; AND
+			(and (funcall fn x) (funcall chain x))))))
+
+(mapcar (fun-ix #'spotted #'evenp) puppies) ; => (NIL NIL NIL NIL NIL)
+
+; anti function intersection, do ANY fns return true?
+(defun fun-ex (fn &rest fns)
+  (if (null fns)
+	  fn
+	  (let ((chain (apply #'fun-ex fns)))
+		#'(lambda (x)
+			; OR
+			(or (funcall fn x) (funcall chain x))))))
+
+(mapcar (fun-ex #'spotted #'evenp) puppies) ; => (T T T T T)
+
+; higher-order list-recurser
+(defun list-rec (rec &optional base)
+  (labels ((self (lst)
+			 (if (null lst)
+				 (if (functionp base)
+					 (funcall base)
+					 base)
+				 (funcall rec (car lst)
+						  #'(lambda () (self (cdr lst)))))))
+	#'self))
+
+; this
+(defun our-length (lst)
+  (if (null lst) 0 (1+ (our-length (cdr lst)))))
+
+; becomes this:
+(setq better-len
+	  (list-rec #'(lambda (next-car recurser) (1+ (funcall recurser))) 0))
+
+(our-length puppies)
+(funcall better-len puppies)
+
+; and this
+(defun our-every (fn lst)
+  (if (null lst)
+	  t
+	  (and (funcall fn (car lst))
+		   (our-every fn (cdr lst)))))
+
+; becomes more specific?
+(setq better-every-odd
+	  (list-rec
+	   #'(lambda (elm f)
+		   (and (oddp elm) (funcall f)))
+	   t))
+
+(our-every #'(lambda (x) (or (evenp x) (oddp x))) puppies)
+(funcall better-every-odd puppies)
+(funcall better-every-odd '(1 3 5))
+
+(setq cpy-list
+	  (list-rec #'(lambda (elm f) (cons x (funcall f)))))
+
+(setq rmv-dups
+	  (list-rec
+	   #'(lambda (elm f) (adjoin x (funcall f)))))
+
+(setq find-if-so
+	  (list-rec
+	   #'(lambda (elm f) (if (fn x) x (funcall f)))))
+
+(setq some-fun
+	  (list-rec
+	   #'(lambda (elm f) (or (fn x) (funcall f)))))
+
+;; TODO: test, left off on PG 70.
