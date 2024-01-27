545b7e8dff6ca738022f990f2fbd7eb4df82f511
K <kelseydrhoda@gmail.com>
2024-01-27 11:51:41 -0700

    Continues In Action

---
+++ beam/elixir/in_action/todo_app/chap6/todo_server.ex
@@ -0,0 +1,137 @@
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+      # Destructures to:
+      # fn entry, todo_list_acc ->
+      # 	add_entry(todo_list_acc, entry)
+      # end
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{
+      todo_list
+      | entries: new_entries,
+        auto_id: todo_list.auto_id + 1
+    }
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  # NOTE: The better way would be to use an Entry struct and spec while we're at it.
+  def update_entry(todo_list, entry_id, updater) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  end
+end
+
+defmodule ServerProcess do
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(request, current_state)
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(request, current_state)
+        loop(callback_module, new_state)
+    end
+  end
+
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self()})
+
+    receive do
+      {:response, response} -> response
+    end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
+  end
+end
+
+defmodule TodoServer do
+  # Follow the protocol
+  def init do
+    TodoList.new()
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:update_entry, old_entry_id, updater}, todo_list) do
+    TodoList.update_entry(todo_list, old_entry_id, updater)
+  end
+
+  def handle_cast({:delete_entry, target_entry_id}, todo_list) do
+    TodoList.delete_entry(todo_list, target_entry_id)
+  end
+
+  def handle_call({:entries, date}, todo_list) do
+    {TodoList.entries(todo_list, date), todo_list}
+  end
+
+  def handle_call({:get_todo_list}, todo_list) do
+    {todo_list, todo_list}
+  end
+
+  # Abstract around the protocol
+
+  def start do
+    ServerProcess.start(TodoServer)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def update_entry(todo_server, old_entry_id, updater) do
+    ServerProcess.cast(todo_server, {:update_entry, old_entry_id, updater})
+  end
+
+  def delete_entry(todo_server, target_entry_id) do
+    ServerProcess.cast(todo_server, {:delete_entry, target_entry_id})
+  end
+
+  def get_todo_list(todo_server) do
+    ServerProcess.call(todo_server, {:get_todo_list})
+  end
+
+  def entries(todo_server, date) do
+    ServerProcess.call(todo_server, {:entries, date})
+  end
+end
